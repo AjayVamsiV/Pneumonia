51d98f05fb616091368f13c3f7e6cce099d5b442
AjayVamsiV <116199429+AjayVamsiV@users.noreply.github.com>
2024-03-21 10:30:44 +0530

    Update stmlit.py

--- stmlit.py
+++ stmlit.py
@@ -4,44 +4,44 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 from pred import efficientNet,resNet
+import base64
 
+#@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.write(' ')
-
-with col2:
-    st.image('images/sircrr.jpeg', width=70)
-
-with col3:
-    st.write(' ')
-    
-header_container = st.container()
-
-header_container.markdown("""
-<style>
-    .header-container {
-        background: linear-gradient(to bottom right, #ff7e5f, #feb47b);
-        display: flex;
-        justify-content: center;
-        align-items: center;
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file) 
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: scroll; # doesn't work
     }
-</style>
-""", unsafe_allow_html=True)
-
-# Create two columns for header elements
-with header_container:
-    #header_col1, header_col2 = st.columns([1, 3])
+    </style>
+    ''' % bin_str
     
-    # Place the image in the first column
-    #with header_col1:
-      # Adjust width as needed
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
-    # Place the header text in the second column and center it
-    #with header_col2:
-    st.markdown('<h1 style="color:#FF1100;text-align:center">Sir C.R.Reddy college of Engineering</h1>', unsafe_allow_html=True)
+
+c1 = st.container()
+with c1:
+    set_png_as_page_bg('images/lung.webp')
+    c1col1, c1col2, c1col3 = st.columns(3)
+    with c1col2:
+        c1c1 = st.container()
+        with c1c1:
+            c1c1col1, c1c1col2,c1c1col3 = st.columns(3)
+            with c1c1col2:
+                st.image('images/sircrr.jpeg', width=70)
+
+
+st.markdown('<h1 style="color:#FF1100;text-align:center">Sir crr college of Engineering</h1>', unsafe_allow_html=True)
 
 st.markdown('<h1 style="color:#4D4DFF;text-align:center">Pneumonia Detection From CT-Scan</h1>', unsafe_allow_html=True)
 left_co, cent_co,last_co = st.columns(3)
@@ -49,7 +49,7 @@ left_co, cent_co,last_co = st.columns(3)
 with cent_co:
     st.image('images/lung.jpg')
 # Header description
-st.markdown('<h2 style="color:gray;text-align:center">This classification model classifies CTScan into Pneumonic and Normal</h2>', unsafe_allow_html=True)
+st.markdown('<h2 style="color:black;text-align:center">This classification model classifies CT-Scan into Pneumonic and Normal</h2>', unsafe_allow_html=True)
 
 upload = st.file_uploader('Upload CT-Scan Image for classification', type=['png', 'jpg'])
 c1, c2 = st.columns(2)
@@ -63,7 +63,7 @@ if upload is not None:
     img2 = np.expand_dims(img2, 0)
     c1.header('Input Image')
     c1.image(im)
-
+    print(img2.shape)
     path1 = 'ResNet.weights.h5'
     path2 = 'EfficientNet.weights.h5'
     model1 = resNet(path1)
@@ -80,5 +80,18 @@ if upload is not None:
         label = "Normal"
     c2.header('Output')
     c2.subheader('Predicted class :')
-    c2.write(label)
-    c2.write(outp[0][0])
+    if(label == 'Pneumonic'):
+        color = 'red'
+    else:
+        color = 'green'
+    
+    formatted_text = f"<span style='color: {color};'>{label}</span>"
+    c2.write(formatted_text, unsafe_allow_html=True)
+    if(label == 'Pneumonic'):
+        text = str(int(outp[0][0]*100))+'% matched with Pneumonic scans'
+        #c2.write(str(int(outp[0][0]*100))+'% matched with Pneumonic scans')
+    else:
+        text = str(100-int(outp[0][0]*100))+'% matched with Normal scans'
+        #c2.write(str(100-int(outp[0][0]*100))+'% matched with Normal scans')
+    formatted_text2 = f"<span style='color: {color};'>{text}</span>"
+    c2.write(formatted_text2, unsafe_allow_html=True)
